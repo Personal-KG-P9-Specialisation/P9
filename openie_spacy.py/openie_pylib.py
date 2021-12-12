837c68c985115d67a8b77a206d296df17baf11f0
Elisabeth Laursen <elaurs17@student.aau.dk>
2021-12-12 18:45:48 +0100

    Added function for coreference resolution from Stanford Core NLP

--- openie_spacy.py/openie_pylib.py
+++ openie_spacy.py/openie_pylib.py
@@ -1,6 +1,7 @@
 from openie import StanfordOpenIE
 import spacy
 import json
+from stanfordcorenlp import StanfordCoreNLP
 
 # Converts list of dictionaries to a string.
 def listToString(s): 
@@ -9,7 +10,13 @@ def listToString(s):
         test = list(ele.values())
         for result in test:
             str1 += (result + " ")
+            # entity_linking(str1)
     return str1 
+
+def openCorpus ():
+    with open('corpus/example-chat.txt', encoding='utf8') as r:
+        corpus = r.read().replace('\n', ' ').replace('\r', '')
+    return corpus
 
 # Prints corpus of triples.
 def printCorpus (triple_corpus):
@@ -30,14 +37,18 @@ def extract_triples ():
         'openie.affinity_probability_cap': 2 / 3,
     }
     with StanfordOpenIE(properties=properties) as client:
-        with open('corpus/example-chat.txt', encoding='utf8') as r:
-            corpus = r.read().replace('\n', ' ').replace('\r', '')
-
+        corpus = openCorpus()
         triples_corpus = client.annotate(corpus)
-
-        # printCorpus(triple_corpus)
         # generate_graph(client, corpus)
-    return triples_corpus
+    return triple_corpus
+
+# Extracts chains of coreferences.
+def coreference_resolution (text):
+    # RUN THIS FIRST FROM NLP FOLDER: java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9001 -timeout 15000 
+    nlp = StanfordCoreNLP('http://localhost', port=9001) # Should update code to only use this not openie lib.
+    # sentence = 'Barack Obama was born in Hawaii.  He is the president. Obama was elected in 2008.'
+    print('Tokenize:', nlp.coref(text))
+    nlp.close()
 
 # Performs entity linking using the Spacy library.
 def entity_linking (text):
@@ -49,9 +60,20 @@ def entity_linking (text):
     for entity in all_linked_entities:
         entity.pretty_print()
 
+# Quick fix to remove duplicate entities before entity linking.
+def el_no_duplicates (triple_corpus):
+    text = listToString(triple_corpus)
+    li = list(text.split(" "))
+    li = list(dict.fromkeys(li))
+    for l in li:
+        entity_linking(l)
 
 if __name__ == '__main__':
-    triple_corpus = extract_triples()
-    text = listToString(triple_corpus)
-    print(text)
-    entity_linking(text)
+    # triple_corpus = extract_triples()
+    # printCorpus(triple_corpus)
+    # text = listToString(triple_corpus)
+    corp = openCorpus()
+    
+    coreference_resolution(corp)
+    # print(text)
+    # entity_linking(text)
