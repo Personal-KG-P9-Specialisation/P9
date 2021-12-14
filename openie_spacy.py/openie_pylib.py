c354e37f49ba1c922cada0834eedbb3cb445c56d
Elisabeth Laursen <elaurs17@student.aau.dk>
2021-12-14 13:01:57 +0100

    Dont touch

--- openie_spacy.py/openie_pylib.py
+++ openie_spacy.py/openie_pylib.py
@@ -2,9 +2,16 @@ from openie import StanfordOpenIE
 import spacy
 import json
 from stanfordcorenlp import StanfordCoreNLP
+import re
+
+def list_to_string(s):
+    str1 = "" 
+    for ele in s: 
+        str1 += ele  
+    return str1 
 
 # Converts list of dictionaries to a string.
-def list_to_string(s): 
+def triple_list_to_string(s): 
     str1 = "" 
     for ele in s:
         test = list(ele.values())
@@ -26,13 +33,13 @@ def print_triple_corpus (triple_corpus):
     print('[...]')
 
 # Generates a graph using Graphviz.
-def openie_graph ():
+def openie_graph (corpus):
     properties = {
         'openie.affinity_probability_cap': 2 / 3,
     }
     with StanfordOpenIE(properties=properties) as client:
-        corpus = open_corpus()
         triple_corpus = client.annotate(corpus)
+        print(triple_corpus)
         graph_image = 'graph.png'
         client.generate_graphviz_graph(corpus, graph_image)
         print('Graph generated: %s.' % graph_image)
@@ -43,7 +50,7 @@ def openie_extract_triples (text):
         'openie.affinity_probability_cap': 2 / 3,
     }
     with StanfordOpenIE(properties=properties) as client:
-        corpus = open_corpus()
+        corpus = text
         triple_corpus = client.annotate(corpus)
     return triple_corpus
 
@@ -52,8 +59,25 @@ def coreference_resolution (text):
     # RUN THIS FIRST FROM NLP FOLDER: java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9001 -timeout 15000 
     nlp = StanfordCoreNLP('http://localhost', port=9001) # Should update code to only use this not openie lib.
     # sentence = 'Barack Obama was born in Hawaii.  He is the president. Obama was elected in 2008.'
-    print('Tokenize:', nlp.coref(text))
+    coref_chains = nlp.coref(text)
     nlp.close()
+    return coref_chains
+
+# Extracts chains of coreferences.
+def triple_integration (text):
+    li = text.replace("!", " ").replace("?", " ").replace(",", " ").replace(".", " ")
+    #li = re.sub(r'[^\w]', ' ', text)
+    #coref_chains = coreference_resolution(text)
+    coref_chains = coreference_resolution(text)
+    for chain in coref_chains:
+        for ref in chain:
+            li = li.replace(ref[3], chain[0][3])
+    
+    print(li)
+    #triples = openie_extract_triples(li)
+    openie_graph(li)
+    #print(triples)
+    #print(coref_chains[0][0][0])
 
 # Performs entity linking using the Spacy library.
 def entity_linking (text):
@@ -67,7 +91,7 @@ def entity_linking (text):
 
 # Quick fix to remove duplicate entities before entity linking.
 def el_no_duplicates (triple_corpus):
-    text = list_to_string(triple_corpus)
+    text = triple_list_to_string(triple_corpus)
     li = list(text.split(" "))
     li = list(dict.fromkeys(li))
     for l in li:
@@ -76,10 +100,10 @@ def el_no_duplicates (triple_corpus):
 if __name__ == '__main__':
     #triple_corpus = extract_triples()
     # print_triple_corpus(triple_corpus)
-    data = open_corpus
-    corp = openie_extract_triples(data)
-    text = list_to_string(corp)
-    print (corp)
+    data = open_corpus()
+    triple_integration(data)
+    #text = triple_list_to_string(corp)
+    #print (corp)
     #corp = open_corpus()
     # openie_graph()
     #coreference_resolution(corp)
