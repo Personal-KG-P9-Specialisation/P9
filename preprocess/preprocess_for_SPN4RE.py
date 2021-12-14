72ab794a27424f0c534c6da030c739dfd394c5c9
abiram98 <34130705+abiram98@users.noreply.github.com>
2021-12-14 10:51:18 +0100

    actual triple extraction data

--- preprocess/preprocess_for_SPN4RE.py
+++ preprocess/preprocess_for_SPN4RE.py
@@ -62,9 +62,38 @@ def find_closest_word(text:str, em:str):
             final_em += new_em[t]
     return final_em
 
-correct_entity_mentions("/home/test/Github/code/SPN4RE/data/WebNLG/clean_WebNLG/valid")
-correct_entity_mentions("/home/test/Github/code/SPN4RE/data/WebNLG/clean_WebNLG/test")
-correct_entity_mentions("/home/test/Github/code/SPN4RE/data/WebNLG/clean_WebNLG/train")
+def filter_trpl_extr_trpls(datafile):
+    data = list()
+    with open("{}.json".format(datafile),"r") as f:
+        for line in f:
+            data.append(json.loads(line))
+    assert len(data) > 0
+    other_f = open("{}_other_v2.json", "w")
+    with open("{}_new_v2.json".format(datafile),"w") as f:
+        for ins in data:
+            new_rels = list()
+            other_rels = list()
+            for rel in ins["relationMentions"]:
+                if not  (rel["em1Text"] in ins["sentText"] or rel["em2Text"] in ins["sentText"]):
+                    other_rels.append({"em1Text" : rel["em1Text"],"em2Text" : rel["em2Text"], "label":rel["label"]})
+                else:
+                    new_rels.append({"em1Text" : rel["em1Text"],"em2Text" : rel["em2Text"], "label":rel["label"]})
+            if len(new_rels) == 0:
+                continue
+            if len(other_rels) > 0:
+                other_f.write(json.dumps({"sentText":ins["sentText"],"relationMentions": other_rels}))
+                other_f.write("\n")
+            f.write(json.dumps({"sentText":ins["sentText"],"relationMentions": new_rels}))
+            f.write("\n")
+    other_f.flush()
+    other_f.close()
+
+filter_trpl_extr_trpls("/home/test/Github/code/SPN4RE/data/WebNLG/clean_WebNLG/valid_new")
+filter_trpl_extr_trpls("/home/test/Github/code/SPN4RE/data/WebNLG/clean_WebNLG/test_new")
+filter_trpl_extr_trpls("/home/test/Github/code/SPN4RE/data/WebNLG/clean_WebNLG/train_new")
+#correct_entity_mentions("/home/test/Github/code/SPN4RE/data/WebNLG/clean_WebNLG/valid")
+#correct_entity_mentions("/home/test/Github/code/SPN4RE/data/WebNLG/clean_WebNLG/test")
+#correct_entity_mentions("/home/test/Github/code/SPN4RE/data/WebNLG/clean_WebNLG/train")
 #one_entity_mention("/home/test/Github/code/SPN4RE/data/WebNLG/clean_WebNLG/valid")        
 #one_entity_mention("/home/test/Github/code/SPN4RE/data/WebNLG/clean_WebNLG/test")
 #one_entity_mention("/home/test/Github/code/SPN4RE/data/WebNLG/clean_WebNLG/train")        
