bd00470ba2b026bfab7b46763493898f96d21fe9
abiram98 <34130705+abiram98@users.noreply.github.com>
2021-12-29 13:31:24 +0100

    itereate dialogues

--- preprocess/sampling.py
+++ preprocess/sampling.py
@@ -5,6 +5,7 @@ class Message:
     def __init__(self, utterance:str, turn:int):
         self.utterance = utterance
         self.turn = turn
+        self.ground_truth = list()
         self.extracted_triple_openIE = list()
         self.criteria_openIE = [0,0]
         self.extracted_triple_SPN4RE = list()
@@ -21,3 +22,25 @@ class Dialogue:
         self.coreference_chain = list()
         self.coreference_resolution_criteria = [0,0]
 
+def read_data(file):
+    dialog_id = 0
+    last =""
+    with open(file,"r") as f:
+        data = f.readlines()
+    for l in data:
+        if l.startswith("your persona"):
+            last = "your persona"
+            continue
+        if l.startswith("partner's persona"):
+            if last == "your persona":
+                dialog_id += 1
+            last = "partner's persona"
+            continue
+        
+    print(dialog_id)
+read_data("../data/ConvAI2/valid_both_original_final.txt")
+"""lst = list()
+lst.append(Message("hello",1))
+lst.append(Message("hi",2))
+d = Dialogue(1,lst)
+print(json.dumps(d,default=lambda a : getattr(a,'__dict__', str(a))))"""
