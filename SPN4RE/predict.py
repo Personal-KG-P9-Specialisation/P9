469a27606e0a391f3cad097241e7b86434b87c4c
abiram98 <34130705+abiram98@users.noreply.github.com>
2021-12-31 16:55:38 +0100

    predict function

--- SPN4RE/predict.py
+++ SPN4RE/predict.py
@@ -3,6 +3,14 @@ import random
 import numpy as np
 from models.setpred4RE import SetPred4RE
 from utils.data import build_data
+from main import str2bool,add_argument_group, get_args,set_seed
+try:
+    from transformers import BertTokenizer
+except:
+    from pytorch_transformers import BertTokenizer
+
+
+
 
 def remove_accents(text: str) -> str:
     accents_translation_table = str.maketrans(
@@ -11,73 +19,11 @@ def remove_accents(text: str) -> str:
     )
     return text.translate(accents_translation_table)
 
-def add_argument_group(name):
-    arg = parser.add_argument_group(name)
-    return arg
-def str2bool(v):
-    return v.lower() in ('true')
+parser = argparse.ArgumentParser()
 
-def get_args():
-    args, unparsed = parser.parse_known_args()
-    if len(unparsed) > 1:
-        print("Unparsed args: {}".format(unparsed))
-    return args, unparsed
-
-
-def set_seed(seed):
-    random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
-
-def prepare_data():
-    st = "hello"
-
-"""
-    def batchify(self, batch_list):
-        batch_size = len(batch_list)
-        sent_idx = [ele[0] for ele in batch_list]
-        sent_ids = [ele[1] for ele in batch_list]
-        targets = [ele[2] for ele in batch_list]
-        sent_lens = list(map(len, sent_ids))
-        max_sent_len = max(sent_lens)
-        input_ids = torch.zeros((batch_size, max_sent_len), requires_grad=False).long()
-        attention_mask = torch.zeros((batch_size, max_sent_len), requires_grad=False, dtype=torch.float32)
-        for idx, (seq, seqlen) in enumerate(zip(sent_ids, sent_lens)):
-            input_ids[idx, :seqlen] = torch.LongTensor(seq)
-            attention_mask[idx, :seqlen] = torch.FloatTensor([1] * seqlen)
-        if self.args.use_gpu:
-            input_ids = input_ids.cuda()
-            attention_mask = attention_mask.cuda()
-            targets = [{k: torch.tensor(v, dtype=torch.long, requires_grad=False).cuda() for k, v in t.items()} for t in targets]
-        else:
-            targets = [{k: torch.tensor(v, dtype=torch.long, requires_grad=False) for k, v in t.items()} for t in targets]
-        info = {"seq_len": sent_lens, "sent_idx": sent_idx}
-        return input_ids, attention_mask, targets, info
-"""
-def tokenise():
-    d = "hello, my name is X"
-
-    pass
 def from_main():
-    parser = argparse.ArgumentParser()
     data_arg = add_argument_group('Data')
-    
-    # data_arg.add_argument('--dataset_name', type=str, default="NYT-exact")
-    # data_arg.add_argument('--train_file', type=str, default="./data/NYT/exact_data/train.json")
-    # data_arg.add_argument('--valid_file', type=str, default="./data/NYT/exact_data/valid.json")
-    # data_arg.add_argument('--test_file', type=str, default="./data/NYT/exact_data/test.json")
-    
-    # data_arg.add_argument('--dataset_name', type=str, default="NYT-partial")
-    # data_arg.add_argument('--train_file', type=str, default="./data/NYT/casrel_data/new_train.json")
-    # data_arg.add_argument('--valid_file', type=str, default="./data/NYT/casrel_data/new_valid.json")
-    # data_arg.add_argument('--test_file', type=str, default="./data/NYT/casrel_data/new_test.json")
-    
-    
+    """python3 -m main --max_epoch 10 --use_gpu False"""
     data_arg.add_argument('--dataset_name', type=str, default="WebNLG")
     data_arg.add_argument('--train_file', type=str, default="./data/WebNLG/clean_WebNLG/train_new_new_v2.json")
     data_arg.add_argument('--valid_file', type=str, default="./data/WebNLG/clean_WebNLG/valid_new_new_v2.json")
@@ -85,14 +31,14 @@ def from_main():
 
     data_arg.add_argument('--generated_data_directory', type=str, default="./data/generated_data/")
     data_arg.add_argument('--generated_param_directory', type=str, default="./data/generated_data/model_param/")
-    data_arg.add_argument('--bert_directory', type=str, default="./bert_base_cased/")
+    data_arg.add_argument('--bert_directory', type=str, default="/home/test/Github/bert/bert-base-cased")
     data_arg.add_argument("--partial", type=str2bool, default=False)
     learn_arg = add_argument_group('Learning')
     learn_arg.add_argument('--model_name', type=str, default="Set-Prediction-Networks")
     learn_arg.add_argument('--num_generated_triples', type=int, default=10)
-    learn_arg.add_argument('--num_decoder_layers', type=int, default=3)
+    learn_arg.add_argument('--num_decoder_layers', type=int, default=4)
     learn_arg.add_argument('--matcher', type=str, default="avg", choices=['avg', 'min'])
-    learn_arg.add_argument('--na_rel_coef', type=float, default=1)
+    learn_arg.add_argument('--na_rel_coef', type=float, default=0.25)
     learn_arg.add_argument('--rel_loss_weight', type=float, default=1)
     learn_arg.add_argument('--head_ent_loss_weight', type=float, default=2)
     learn_arg.add_argument('--tail_ent_loss_weight', type=float, default=2)
@@ -100,15 +46,15 @@ def from_main():
     learn_arg.add_argument('--batch_size', type=int, default=8)
     learn_arg.add_argument('--max_epoch', type=int, default=50)
     learn_arg.add_argument('--gradient_accumulation_steps', type=int, default=1)
-    learn_arg.add_argument('--decoder_lr', type=float, default=2e-5)
-    learn_arg.add_argument('--encoder_lr', type=float, default=1e-5)
-    learn_arg.add_argument('--lr_decay', type=float, default=0.01)
-    learn_arg.add_argument('--weight_decay', type=float, default=1e-5)
-    learn_arg.add_argument('--max_grad_norm', type=float, default=0)
+    learn_arg.add_argument('--decoder_lr', type=float, default=0.00005)
+    learn_arg.add_argument('--encoder_lr', type=float, default=0.00002)
+    learn_arg.add_argument('--lr_decay', type=float, default=0.02)
+    learn_arg.add_argument('--weight_decay', type=float, default=0.000001)
+    learn_arg.add_argument('--max_grad_norm', type=float, default=20)
     learn_arg.add_argument('--optimizer', type=str, default='AdamW', choices=['Adam', 'AdamW'])
     evaluation_arg = add_argument_group('Evaluation')
     evaluation_arg.add_argument('--n_best_size', type=int, default=100)
-    evaluation_arg.add_argument('--max_span_length', type=int, default=12) #NYT webNLG 10
+    evaluation_arg.add_argument('--max_span_length', type=int, default=10)
     misc_arg = add_argument_group('MISC')
     misc_arg.add_argument('--refresh', type=str2bool, default=False)
     misc_arg.add_argument('--use_gpu', type=str2bool, default=True)
@@ -122,12 +68,8 @@ def from_main():
     set_seed(args.random_seed)
     return args
 args = from_main()
-try:
-    from transformers import BertTokenizer
-except:
-    from pytorch_transformers import BertTokenizer
 
-def predict_triples(utterance, model, tokenizer, data):
+def predict_triples(utterance, model, tokenizer, relation_alphabet):
     token_sent = [tokenizer.cls_token] + tokenizer.tokenize(remove_accents(utterance)) + [tokenizer.sep_token]
     sent_ids = tokenizer.convert_tokens_to_ids(token_sent)
     max_sent_len = len(sent_ids)
@@ -137,12 +79,31 @@ def predict_triples(utterance, model, tokenizer, data):
     attention_mask[0, :max_sent_len] = torch.FloatTensor([1] * max_sent_len)
     info ={"seq_len":[max_sent_len],"sent_idx":[0]}
     output = model.gen_triples(input_ids, attention_mask, info)
-    relation = output[0][0][0]
-    relation = data.relational_alphabet.get_instance(relation)
+    triples = list()
+    for i in range(len(output[0])):
+        relation = output[0][i].pred_rel
+        relation = relation_alphabet.get_instance(relation)
+        head_s = output[0][i].head_start_index
+        head_e = output[0][i].head_end_index
+        tail_s = output[0][i].tail_start_index
+        tail_e = output[0][i].tail_end_index
+        head = token_sent[head_s:head_e+1]
+        head = " ".join([x for x in head]).replace(" ##","")
+        tail = token_sent[tail_s:tail_e+1]
+        tail = " ".join([x for x in tail]).replace(" ##","")
+        triples.append((head,relation,tail))
+    return triples
 
+def load_model(path_model, args):
+    model = SetPred4RE(args, 61)
+    state_dict = torch.load(path_model)
+    model.load_state_dict(state_dict["state_dict"])
+    return model
 
-    pass
 tokenizer = BertTokenizer.from_pretrained("BERT DIR", do_lower_case=False)
 data = build_data(args)
-model = SetPred4RE(args, data.relational_alphabet.size())
-
+model = load_model("/home/test/Github/code/SPN4RE/data/generated_data/model_param/SetPred4RE_WebNLG_epoch_0_f1_0.3946.model",args)
+#SetPred4RE(args, data.relational_alphabet.size())
+#test
+utterance = "my mom had me in mcdonalds ."
+triples = predict_triples(utterance,model,tokenizer,data.relational_alphabet)
